6cbfed3a3e9f49c5fb2b52e32a09c6b02ee2b825
thamer <themerlefi@gmail.com>
2023-06-28 06:47:02 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <footer className="text-center text-lg-start  text-muted mt-2">
+      <footer className="text-center text-lg-start text-white text-muted mt-2">
         <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
           <div className="me-5 d-none d-lg-block">
             <span>Get connected with us on social networks:</span>
